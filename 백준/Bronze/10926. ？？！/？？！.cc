9a45def71c64335e3d75aae7791cfaca901fd5df
cruelart <48382001+cruelart@users.noreply.github.com>
2025-06-01 00:35:42 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+	string str;
+	cin >> str;
+
+	cout << str << "??!";
+
+	return 0;
+}
