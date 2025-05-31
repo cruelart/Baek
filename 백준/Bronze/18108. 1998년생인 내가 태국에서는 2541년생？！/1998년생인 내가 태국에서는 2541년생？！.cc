801a2aa37127d06db7b0505db9cfa8fe4b9a3ce8
cruelart <48382001+cruelart@users.noreply.github.com>
2025-06-01 00:38:07 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+	int other_year;
+	cin >> other_year;
+
+	cout << other_year - (2541 - 1998);
+
+	return 0;
+}
