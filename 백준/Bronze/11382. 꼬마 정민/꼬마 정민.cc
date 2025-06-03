e7910f9e26104c4145003c0711d37d06d32aaa1e
cruelart <48382001+cruelart@users.noreply.github.com>
2025-06-03 19:39:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+	long long result = 0;
+
+	long long A, B, C;
+	cin >> A >> B >> C;
+
+	result += A + B + C;
+
+	cout << result;
+
+	return 0;
+}
