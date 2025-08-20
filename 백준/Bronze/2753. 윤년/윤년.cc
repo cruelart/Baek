b2b1c316d468703eba5f9cf97329d34b3704b673
cruelart <48382001+cruelart@users.noreply.github.com>
2025-08-21 06:14:53 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int year;
+	cin >> year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				cout << '1';
+				return 0;
+			}
+
+			cout << '0';
+			return 0;
+		}
+		cout << '1';
+		return 0;
+	}
+
+	cout << '0';
+}
