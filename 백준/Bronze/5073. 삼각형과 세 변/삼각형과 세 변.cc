1ccc4ff9f7d363f1d787b3102bfeef8590262fb8
cruelart <48382001+cruelart@users.noreply.github.com>
2025-09-22 13:31:36 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,38 +12,24 @@ int main()
 	{
 		int side_length[3];
 		cin >> side_length[0] >> side_length[1] >> side_length[2];
+		sort(side_length, side_length + 3);
 
-		pair<int,int> max_length = pair<int,int>(0,side_length[0]); // 인덱스 번호, 인덱스값
 		int withoutMax_sum = 0;
 
-		for (int i = 0; i < 3; i++)
-		{
-			if (max_length.second < side_length[i])
-			{
-				max_length.first = i;
-				max_length.second = side_length[i];
-			}
-		}
-		for (int k = 0; k < 3; k++)
-		{
-			if (k != max_length.first)
-			{
-				withoutMax_sum += side_length[k];
-			}
-		}
+		withoutMax_sum += side_length[0] + side_length[1];
 
-		if (max_length.second == 0)
+		if (side_length[2] == 0)
 		{
 			break;
 		}
 
-		if (max_length.second >= withoutMax_sum)
+		if (side_length[2] >= withoutMax_sum)
 		{
 			cout << "Invalid" << '\n';
 		}
-		else if (side_length[0] == side_length[1] || side_length[1] == side_length[2] || side_length[2] == side_length[0])
+		else if (side_length[0] == side_length[1] || side_length[1] == side_length[2])
 		{
-			if (side_length[0] == side_length[1] && side_length[1] == side_length[2] && side_length[2] == side_length[0])
+			if (side_length[0] == side_length[1] && side_length[1] == side_length[2])
 			{
 				cout << "Equilateral" << '\n';
 				continue;
