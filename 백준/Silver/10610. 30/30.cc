d79d5d00712d9256697c3080ef4c8f16330f1fec
cruelart <48382001+cruelart@users.noreply.github.com>
2026-01-05 19:02:39 +0900

    [Silver IV] Title: 30, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -3,30 +3,36 @@
 
 using namespace std;
 
-int main() {
-    string N;
-    cin >> N;
+int main()
+{
+	string N;
+	cin >> N;
 
-    // 0이 없으면 불가능
-    if (N.find('0') == string::npos) {
-        cout << -1;
-        return 0;
-    }
+	if (N.find('0') == N.npos)
+	{
+		cout << -1;
+		return 0;
+	}
+	else
+	{
+		swap(N[N.length()-1], N[N.find('0')]);
+	}
 
-    // 전체 숫자 합 계산
-    int sum = 0;
-    for (char c : N)
-        sum += c - '0';
+	int sum = 0;
 
-    // 3의 배수 확인
-    if (sum % 3 != 0) {
-        cout << -1;
-        return 0;
-    }
+	for (int index = 0; index < N.length() - 1; index++)
+	{
+		sum += N[index] - '0';
+	}
 
-    // 내림차순 정렬
-    sort(N.begin(), N.end(), greater<char>());
-
-    cout << N;
-    return 0;
+	if (sum % 3 == 0)
+	{
+		sort(N.begin(), N.end() - 1, [](const int& a, const int& b) {return a > b; });
+		cout << N;
+	}
+	else
+	{
+		cout << -1;
+	}
+	return 0;
 }
