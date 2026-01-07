37d4a1ffeeedf6bdd79b55dedd2d3f8dd34ff155
cruelart <48382001+cruelart@users.noreply.github.com>
2026-01-08 05:53:25 +0900

    [Gold IV] Title: 강의실 배정, Time: 52 ms, Memory: 6816 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -7,47 +7,43 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+	int N;
+	cin >> N;
 
-    vector<pair<long long, long long>> lec(N);
-    // lec[i].first  = 시작시간
-    // lec[i].second = 끝시간
+	vector<pair<long long, long long>> start_end(N);
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> lec[i].first >> lec[i].second;
-    }
+	for (int n = 0; n < N; n++)
+	{
+		cin >> start_end[n].first >> start_end[n].second;
+	}
 
-    // 시작시간 기준 정렬
-    sort(lec.begin(), lec.end());
+	sort(start_end.begin(), start_end.end()); //오름차순으로 정렬
 
-    // 끝나는 시간만 저장하는 최소 힙
-    priority_queue<long long, vector<long long>, greater<>> pq;
+	priority_queue<long long, vector<long long>, greater<>> end_queue;
 
-    // 첫 수업의 끝시간 push
-    pq.push(lec[0].second);
+	end_queue.push(start_end[0].second);
 
-    for (int i = 1; i < N; i++)
-    {
-        long long earliestEnd = pq.top();
 
-        // 가장 빨리 끝나는 강의실에 이어서 쓸 수 있으면
-        if (lec[i].first >= earliestEnd)
-        {
-            pq.pop();
-            pq.push(lec[i].second);
-        }
-        else
-        {
-            // 새 강의실 필요
-            pq.push(lec[i].second);
-        }
-    }
+	for (int n = 1; n < N; n++)
+	{
+		long long end_time = end_queue.top();
 
-    cout << pq.size();
-    return 0;
+		if (start_end[n].first >= end_time)
+		{
+			end_time = start_end[n].second;
+			end_queue.pop();
+			end_queue.push(end_time);
+		}
+		else // 겹친다면
+		{
+			end_queue.push(start_end[n].second);
+		}
+	}
+
+	cout << end_queue.size();
+
+	return 0;
 }
